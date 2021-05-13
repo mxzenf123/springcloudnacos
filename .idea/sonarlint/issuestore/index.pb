993a8091898cfde9b644d341777c992fccb2d7c0
mxzenf123 <93839187@qq.com>
2021-05-13 16:35:36 +0800

    初始化

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+_
+/provider/src/test/java/org/yangxin/AppTest.java,6\2\62c2ba52c6e11b2cbdad688e135246214150ebd7
+@
+provider/pom.xml,2\d\2d4b0002f4d67b39ea06ac3861afaaa7b3f83dc0
+@
+consumer/pom.xml,a\e\ae0fe1fce8f97995f0225aa73104a92a194ef0d5
+[
++provider/src/main/java/org/yangxin/App.java,0\8\087e6f92713e53eff4d05fc2e4417fb982a5adc4
+[
++consumer/src/main/java/org/yangxin/App.java,7\1\7114ec0df28de6432f6b3d84bff96191b3de48eb
---
+++ consumer/src/main/java/org/yangxin/App.java
@@ -0,0 +1,54 @@
+package org.yangxin;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @Author
+ * 消费者
+ *
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        SpringApplication.run(App.class,args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+
+        return new RestTemplate();
+    }
+
+    @RestController
+    public class NacosController{
+
+        @Autowired
+        private LoadBalancerClient loadBalancerClient;
+        @Autowired
+        private RestTemplate restTemplate;
+
+        private String appName = "consumer";
+
+        @GetMapping("/echo/app-name")
+        public String echoAppName(){
+            ServiceInstance serviceInstance = loadBalancerClient.choose("yinhai-provider");
+            String path = String.format("http://%s:%s/echo/%s",serviceInstance.getHost(),serviceInstance.getPort(),appName);
+            System.out.println("request path:" +path);
+            return restTemplate.getForObject(path,String.class);
+        }
+
+    }
+}
